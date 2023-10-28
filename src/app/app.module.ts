d16c79d6fb4c5e98247e609ca2ce46a07d5cb257
Reza Mahmoodi <en.rezamahmoodi@gmail.com>
2023-10-28 13:21:01 +0330

    *

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
 import { GamePageComponent } from './pages/game-page/game-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamePageComponent
+    GamePageComponent,
+    QuestionComponent,
   ],
   imports: [
     BrowserModule,
