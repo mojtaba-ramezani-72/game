1cfd4d44b599c7274381c7d51fdb78941b2c9c3f
Reza Mahmoodi <en.rezamahmoodi@gmail.com>
2023-10-28 16:32:21 +0330

    + guard

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -1,22 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Login, Register } from "../../model/User.model";
-import { Observable } from "rxjs/internal/Observable";
+import { Observable } from 'rxjs/internal/Observable';
+import { Login, Register } from '../../model/User.model';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:8000/users';
+	private baseUrl = 'http://localhost:8000/users';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  register(user: Register) {
-    return this.http.post<any>(this.baseUrl, user);
-  }
+	register(user: Register) {
+		return this.http.post<any>(this.baseUrl, user);
+	}
 
-  login(): Observable<Array<Login>> {
-    return this.http.get<any>(this.baseUrl);
-  }
-
+	login(): Observable<Array<Login>> {
+		return this.http.get<any>(this.baseUrl);
+	}
 }
--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -1,41 +1,44 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { AuthService } from "../auth.service";
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StorageService } from '../../../services/storage.service';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+	constructor(private router: Router, private authService: AuthService, private storageService: StorageService) {}
 
-  constructor(private authService: AuthService) {}
+	loginForm: FormGroup = new FormGroup({
+		email: new FormControl(''),
+		password: new FormControl(''),
+	});
 
-  loginForm: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+	onSubmit() {
+		this.authService.login().subscribe({
+			complete: () => {},
+			error: () => {
+				alert('something was wrong');
+			},
+			next: (res) => {
+				const { email, password } = this.loginForm.value;
+				const user = res.find((a: any) => {
+					return a.email === email && a.password === password;
+				});
 
-  onSubmit() {
-    this.authService.login().subscribe({
-      complete: () => {},
-      error: () => {
-        alert('something was wrong');
-      },
-      next: (res) => {
-        const user = res.find((a: any) => {
-          const { email, password } = this.loginForm.value;
-          return a.email === email && a.password === password;
-        });
+				console.log('user: ', user);
+				if (user) {
+					this.storageService.setItem('userinfo', { email });
+					this.storageService.setItem('token', btoa(email));
 
-        if (user) {
-          alert("you are successfully login :)")
-        } else {
-          alert("user Not Found")
-        }
-
-      },
-    })
-  }
-
+					this.router.navigate(['game-page']);
+				} else {
+					alert('user Not Found');
+				}
+			},
+		});
+	}
 }
