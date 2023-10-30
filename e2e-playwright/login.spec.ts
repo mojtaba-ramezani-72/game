0e4154e6e4b12db4c2f993c7467cc9db03ac24e0
Mojtaba Ramezani <ramezanimojtaba72@gmail.com>
2023-10-30 16:27:27 +0330

    *

--- e2e-playwright/login.spec.ts
+++ e2e-playwright/login.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('test login', async ({ page }) => {
-  await page.goto('http://localhost:4200/login-page');
+  await page.goto('http://localhost:4200/login');
   await page.locator('[data-test="email"]').click();
   await page.locator('[data-test="email"]').fill('player@gmail.com');
   await page.locator('[data-test="email"]').press('Tab');
--- e2e-playwright/register.spec.ts
+++ e2e-playwright/register.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('test register', async ({ page }) => {
-  await page.goto('http://localhost:4200/register-page');
+  await page.goto('http://localhost:4200/register');
   await page.locator('[data-test="email"]').click();
   await page.locator('[data-test="email"]').fill('player2@gmail.com');
   await page.locator('[data-test="email"]').press('Tab');
